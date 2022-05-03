04659d6d2255b4501590bd4087ba9d2f88f808e1
Marcoantonio Manso <83654329+marcomansomm@users.noreply.github.com>
2022-05-03 10:53:37 -0300

    Fazendo Mudanças no código

--- calculoMatriz.c
+++ calculoMatriz.c
@@ -1,42 +1,63 @@
 #include <stdio.h>
 #include <pthread.h>
+#include <stdlib.h>
 
-int somaGeral = 0;
+int somaTotal = 0;
+int *elementos;
+int tam = 0;
+int inicio = 0;
+int termino;
 
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
-void *somaGeral (int soma);
+void *somarLinhas (void *arg);
 
 int main(void){
-	int linha, coluna = 0;
-
-	scanf("%d", linha);
-	coluna = linha;
-
+	int linha = 0, coluna = 0, k = 0;
+	
+  	printf("Digite aqui o tamanho da sua matriz> ");
+	scanf("%d ", &tam);
+	
+	termino = tam;
+	coluna = linha = tam;
+	
 	int matriz[linha][coluna];
-	pthread_t threads[linha];
 
-	for(int i = 0; i<linha; i++){
-		if(pthread_create(&(threads[i]), NULL, somaGeral, soma)){
+	elementos = malloc(sizeof(int) * (linha * coluna));
+
+	pthread_t threads[tam];
+
+	for(int i = 0; i<tam; i++){
+		for(int j = 0; j<tam; j++){
+			scanf(" %d", &elementos[k]);
+			matriz[i][j] = elementos[j];
+			k++;
+		}
+	}
+
+	for(int i = 0; i<tam; i++){
+		if(pthread_create(&(threads[i]), NULL, somarLinhas, NULL)){
 			printf("A thread %d não foi criada!", i);
 		}
 	}
 
-	for(int i = 0; i<coluna; i++){
-		for(int j = 0; j<linha; j++){
-			scanf("%d", elementos);
-			pthread_join(threads[i], NULL);
-			soma += elementos;
-		}
-		soma = 0;
+	for(int i = 0; i<tam; i++){
+		pthread_join(threads[i], NULL);
 	}
 
+	printf("%d", somaTotal);
 	return 0;
 }
 
-void *somaGeral(int soma){
+void *somarLinhas(void *arg){
 	pthread_mutex_lock(&mutex);
-	somaGeral += soma;
+	
+	for(int i = inicio; i < termino; i++){
+		somaTotal += elementos[i];
+	}
+	inicio = termino;
+	termino += tam;
+	
 	pthread_mutex_unlock(&mutex);
-
+	return arg;
 }
